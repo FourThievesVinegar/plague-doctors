5e912d0c5322a6f5c89fad163af10abd9f6dc0c4
Mike Kissinger <mis4mike@gmail.com>
2022-03-09 19:01:14 -0500

    tweak whacking speed

--- plague-doctors.js
+++ plague-doctors.js
@@ -44,7 +44,7 @@ const COLOR_MAP = {
 };
 const HEATBEAT_INTERVAL = 100;
 const HEARTBEATS_TO_DIE = 12;
-const HEARTBEATS_TO_WHACK = 12;
+const HEARTBEATS_TO_WHACK = 8;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
